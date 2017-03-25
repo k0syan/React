2ecce52fa903d059b586326cec1a020accf191b5
shahenkosyan <shahen.qosyan@gmail.com>
2017-03-25 19:08:03 +0400

    Update Knight.js

---
+++ dnd/src/Knight/Knight.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Shahen Kosyan on 3/25/17.
+ */
+
+import React, { Component } from 'react';
+import './Knight.css';
+
+class Knight extends Component {
+  render() {
+    return <span className="knight">♘</span>;
+  }
+}
+
+export default Knight;
